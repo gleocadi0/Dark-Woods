b9eebde73ec38c50850c89e83138de103a138355
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-07-15 00:25:48 -0300

    WIP

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
 
-    void Start() { }
-
-    void Update()
+    private void Update()
     {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        transform.Translate(x, 0, z);
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(x, 0, 0);
+
+        if (movement.x > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (movement.x < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        transform.position += movement * speed * Time.deltaTime;
     }
 }
